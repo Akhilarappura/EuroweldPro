3b9f23811cb231b60c471f2ce2e2f3717ca4c5ad
Akhilarappura <itsmeakhilarappura@gmail.com>
2025-07-31 08:28:16 +0530

    layout changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'EuroWeld',
+  description: 'manpowerSupply',
+  generator: 'celestial code',
 }
 
 export default function RootLayout({
